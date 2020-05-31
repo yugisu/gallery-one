d77ee78989748fdde6e7cd6d2e3ff22571e52195
Dmytrii Puzyr <slymahel@gmail.com>
2020-05-31 12:53:22 +0300

    Add fonts

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -3,6 +3,8 @@ import { createGlobalStyle } from 'styled-components';
 export const GlobalStyles = createGlobalStyle`
   html {
     box-sizing: border-box;
+    font-family: 'Roboto', sans-serif;
+    color: white;
   }
 
   *,
